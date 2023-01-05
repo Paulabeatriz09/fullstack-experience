e27df5d8ead0f158c72022496894b340db2a01a0
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2023-01-05 14:15:19 -0300

    segundo

---
+++ E02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class E02 {
+    public static void main(String[] args){
+
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Insira a quantidade funcionarios :");
+        double n1=entrada.nextInt();
+        System.out.println("Insira a quantidade de horas : ");
+        double n2=entrada.nextInt();
+        System.out.println("Insira o valor da hora : ");
+        double n3=entrada.nextInt();
+
+        double salario=n2*n3;
+
+        System.out.println("Número :"+n1+" Salário : "+salario);
+
+
+
+    
+}
+}
